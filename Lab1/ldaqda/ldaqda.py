dec0c2ea6aca553931cebfdbc115b1cdf4ec256c
Michael Vu <michael.vu.97@outlook.com>
2020-02-20 09:43:35 -0500

    Started LDA/QDA

--- Lab1/ldaqda/ldaqda.py
+++ Lab1/ldaqda/ldaqda.py
@@ -20,9 +20,38 @@ def discrimAnalysis(x, y):
     Besides producing the five outputs, you need also to plot 1 figure for LDA 
     and 1 figure for QDA in this function         
     """
-    ### TODO: Write your code here
-    
-    
+    x_male = []
+    x_female = []
+    for i in range(len(y)):
+        if y[i] == 1:
+            x_male.append(x[i])
+        else:
+            x_female.append(x[i])
+
+    mu_male = np.average(x_male)
+    mu_female = np.average(x_female)
+
+    mu = np.average(x)
+    cov = np.average(np.matmul((x - mu),np.transpose(x - mu)))
+
+    cov_male = np.average(np.matmul(x_male - mu_male, np.transpose(x_male - mu_male)))
+    cov_female = np.average(np.matmul(x_female - mu_female, np.transpose(x_female - mu_female)))
+
+    male_dist_mesh = np.meshgrid(np.linspace(50, 80), np.linspace(80,280))
+    female_dist_mesh = np.meshgrid(np.linspace(50, 80), np.linspace(80,280))
+
+    # Visualization
+    fig = plt.figure()
+    ax1 = fig.add_subplot("111")
+    ax1.plot(np.array(x_male)[...,0], np.array(x_male)[...,1], 'bs', label='male')
+    ax1.plot(np.array(x_female)[...,0], np.array(x_female)[...,1], 'ro', label='female')
+    plt.xlabel("height")
+    plt.ylabel("weight")
+    plt.legend()
+    plt.xlim(50, 80)
+    plt.ylim(80,280)
+    plt.show()
+
     return (mu_male,mu_female,cov,cov_male,cov_female)
     
 
